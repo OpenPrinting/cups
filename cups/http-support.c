49a862b415b94f9095df05781ec4372e710a6b65
Zdenek Dohnal <zdohnal@redhat.com>
2020-11-25 13:24:28 +0100

    http-support.c: Add avahi-common/malloc.h header for avahi_free()

--- cups/http-support.c
+++ cups/http-support.c
@@ -27,6 +27,7 @@
 #elif defined(HAVE_AVAHI)
 #  include <avahi-client/client.h>
 #  include <avahi-client/lookup.h>
+#  include <avahi-common/malloc.h>
 #  include <avahi-common/simple-watch.h>
 #endif /* HAVE_DNSSD */
 
