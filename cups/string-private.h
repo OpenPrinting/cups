e6ace2894e77ca2a15dd03f8c5d63cca0da43b05
Michael R Sweet <msweet@msweet.org>
2021-03-04 09:37:28 -0500

    Work on modernizing autoconf source files and the build system:
    - Adopt non-deprecated autoconf macros, wrap shell "if" and "case" statements
      with AS_IF and AS_CASE macros.
    - Remove strings.h and bstring.h checks since they are not needed for modern
      OS's.
    - Rename cups-ssl.m4 to cups-tls.m4 (TLS support, not SSL support)

--- cups/string-private.h
+++ cups/string-private.h
@@ -19,6 +19,7 @@
 #  include <stdio.h>
 #  include <stdlib.h>
 #  include <stdarg.h>
+#  include <string.h>
 #  include <ctype.h>
 #  include <errno.h>
 #  include <locale.h>
@@ -26,17 +27,6 @@
 
 #  include <cups/versioning.h>
 
-#  ifdef HAVE_STRING_H
-#    include <string.h>
-#  endif /* HAVE_STRING_H */
-
-#  ifdef HAVE_STRINGS_H
-#    include <strings.h>
-#  endif /* HAVE_STRINGS_H */
-
-#  ifdef HAVE_BSTRING_H
-#    include <bstring.h>
-#  endif /* HAVE_BSTRING_H */
 
 #  if defined(_WIN32) && !defined(__CUPS_SSIZE_T_DEFINED)
 #    define __CUPS_SSIZE_T_DEFINED
