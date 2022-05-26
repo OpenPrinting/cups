411b6136f450a583ee08c3880fa09dbe837eb3f1
Zdenek Dohnal <zdohnal@redhat.com>
2022-05-26 09:28:20 +0200

    scheduler/cert.c: fix the comment

--- scheduler/cert.c
+++ scheduler/cert.c
@@ -447,7 +447,7 @@ ctcompare(const char *a,		/* I - First string */
  /*
   * The while loop finishes when *a == '\0' or *b == '\0'
   * so after the while loop either both *a and *b == '\0',
-  * or one points inside a string, so when we apply logical OR on *a,
+  * or one points inside a string, so when we apply bitwise OR on *a,
   * *b and result, we get a non-zero return value if the compared strings don't match.
   */
 
