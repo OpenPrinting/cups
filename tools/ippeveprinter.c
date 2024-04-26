fc04a4273f0d9b88d2bf9aebeb90353206aeeff9
Zdenek Dohnal <zdohnal@redhat.com>
2024-04-26 11:52:21 +0200

    Fix ippeveprinter issues

--- tools/ippeveprinter.c
+++ tools/ippeveprinter.c
@@ -4096,14 +4096,20 @@ ippserver_attr_cb(
     "identify-actions-default",
     "identify-actions-supported",
     "ipp-features-supported",
-    "ipp-versions-supproted",
+    "ipp-versions-supported",
     "ippget-event-life",
+    "job-creation-attributes-supported",
     "job-hold-until-supported",
     "job-hold-until-time-supported",
     "job-ids-supported",
     "job-k-octets-supported",
+    "job-priority-default",
+    "job-priority-supported",
     "job-settable-attributes-supported",
+    "job-sheets-default",
+    "job-sheets-supported",
     "multiple-document-jobs-supported",
+    "multiple-document-handling-supported",
     "multiple-operation-time-out",
     "multiple-operation-time-out-action",
     "natural-language-configured",
@@ -4115,6 +4121,7 @@ ippserver_attr_cb(
     "notify-max-events-supported",
     "notify-pull-method-supported",
     "operations-supported",
+    "pdl-override-supported",
     "printer-alert",
     "printer-alert-description",
     "printer-camera-image-uri",
@@ -4126,18 +4133,25 @@ ippserver_attr_cb(
     "printer-detailed-status-messages",
     "printer-dns-sd-name",
     "printer-fax-log-uri",
+    "printer-geo-location",
     "printer-get-attributes-supported",
     "printer-icons",
     "printer-id",
     "printer-info",
     "printer-is-accepting-jobs",
+    "printer-location",
     "printer-message-date-time",
     "printer-message-from-operator",
     "printer-message-time",
     "printer-more-info",
+    "printer-name",
+    "printer-organization",
+    "printer-organizational-unit",
     "printer-service-type",
     "printer-settable-attributes-supported",
     "printer-state",
+    "printer-state-change-date-time",
+    "printer-state-change-time",
     "printer-state-message",
     "printer-state-reasons",
     "printer-static-resource-directory-uri",
@@ -4148,6 +4162,7 @@ ippserver_attr_cb(
     "printer-supply-info-uri",
     "printer-up-time",
     "printer-uri-supported",
+    "printer-uuid",
     "printer-xri-supported",
     "queued-job-count",
     "reference-uri-scheme-supported",
@@ -7500,7 +7515,7 @@ respond_http(
     * 100-continue doesn't send any headers...
     */
 
-    return (httpWriteResponse(client->http, HTTP_STATUS_CONTINUE) == 0);
+    return (!httpWriteResponse(client->http, HTTP_STATUS_CONTINUE));
   }
 
  /*
@@ -7549,7 +7564,7 @@ respond_http(
 
   httpSetLength(client->http, length);
 
-  if (httpWriteResponse(client->http, code) < 0)
+  if (httpWriteResponse(client->http, code))
     return (0);
 
  /*
