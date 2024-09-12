2dad30d9476b19279702aafcbb754fc613ab014a
Michael R Sweet <msweet@msweet.org>
2024-09-12 13:23:50 -0400

    Merge ipptool fixes and improvements, update man pages.

--- tools/ipptool.c
+++ tools/ipptool.c
@@ -6632,7 +6632,8 @@ with_content(
   char		scheme[256],		// Scheme
 		userpass[256],		// Username:password (not used)
 		host[256],		// Hostname
-		resource[256];		// Resource path
+		resource[256],		// Resource path
+		*resptr;		// Pointer into resource
   int		port;			// Port number
   http_encryption_t encryption;		// Encryption  mode
   http_uri_status_t uri_status;		// URI decoding status
@@ -6651,6 +6652,9 @@ with_content(
       ret = false;
       continue;
     }
+
+    if ((resptr = strchr(resource, '#')) != NULL)
+      *resptr = '\0';			// Strip HTML target ("...#target")
 
     if (strcmp(scheme, "http") && strcmp(scheme, "https") && strcmp(scheme, "ipp") && strcmp(scheme, "ipps"))
     {
@@ -6721,7 +6725,7 @@ with_content(
 
       if (status != HTTP_STATUS_OK)
       {
-        add_stringf(errors, "Got unexpected status %d for HEAD request to '%s'.", (int)status, uri);
+        add_stringf(errors, "Got unexpected status %d for GET request to '%s'.", (int)status, uri);
         ret = false;
         goto get_done;
       }
@@ -6765,14 +6769,38 @@ with_content(
       }
       else if (!_cups_strcasecmp(content_type, "image/jpeg") || !_cups_strcasecmp(content_type, "image/png"))
       {
+        // Validate image content
         if (!valid_image(filename, &width, &height, &depth))
         {
-	  add_stringf(errors, "Unable to load image '%s'.", uri);
+	  add_stringf(errors, "Unable to open image '%s'.", uri);
 	  ret = false;
 	  goto get_done;
         }
       }
-      else if (!_cups_strcasecmp(content_type, "application/pdf") || !_cups_strcasecmp(content_type, "application/ipp") || !_cups_strcasecmp(content_type, "application/vnd.iccprofile") || !_cups_strcasecmp(content_type, "text/css") || !_cups_strcasecmp(content_type, "text/html") || !_cups_strcasecmp(content_type, "text/strings"))
+      else if (!_cups_strcasecmp(content_type, "application/ipp"))
+      {
+        ipp_t	*ipp = ippNew();	// IPP message
+
+        if ((fd = open(filename, O_RDONLY | O_BINARY)) < 0)
+        {
+          add_stringf(errors, "Unable to open '%s': %s", uri, strerror(errno));
+          ippDelete(ipp);
+          ret = false;
+          goto get_done;
+        }
+        else if (ippReadFile(fd, ipp) != IPP_STATE_DATA)
+        {
+          add_stringf(errors, "Unable to read '%s': %s", uri, cupsGetErrorString());
+          ippDelete(ipp);
+          close(fd);
+          ret = false;
+          goto get_done;
+        }
+
+	ippDelete(ipp);
+	close(fd);
+      }
+      else if (!_cups_strcasecmp(content_type, "application/pdf") || !_cups_strcasecmp(content_type, "application/vnd.iccprofile") || !_cups_strcasecmp(content_type, "text/css") || !_cups_strcasecmp(content_type, "text/html") || !_cups_strncasecmp(content_type, "text/html;", 10) || !_cups_strcasecmp(content_type, "text/strings"))
       {
         // Just require these files to be non-empty for now, might add more checks in the future...
         if (fileinfo.st_size == 0)
@@ -6784,7 +6812,7 @@ with_content(
       }
       else
       {
-	add_stringf(errors, "Got unexpected Content-Type '%s' for HEAD request to '%s'.", content_type, uri);
+	add_stringf(errors, "Got unexpected Content-Type '%s' for GET request to '%s'.", content_type, uri);
 	ret = false;
 	goto get_done;
       }
