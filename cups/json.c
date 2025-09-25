9d09e60f56f74b2089aa38449d65fae2c71f6b38
Michael R Sweet <msweet@msweet.org>
2025-09-25 09:52:43 -0400

    Bump copyright.

--- cups/json.c
+++ cups/json.c
@@ -1,7 +1,7 @@
 //
 // JSON API implementation for CUPS.
 //
-// Copyright © 2022-2024 by OpenPrinting.
+// Copyright © 2022-2025 by OpenPrinting.
 //
 // Licensed under Apache License v2.0.  See the file "LICENSE" for more
 // information.
