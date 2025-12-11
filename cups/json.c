cb7165ba4ee40e59c4c402298ab3c143b8217c05
Michael R Sweet <msweet@msweet.org>
2025-12-11 11:32:57 -0500

    Fix speling (Issue #1452)

--- cups/json.c
+++ cups/json.c
@@ -299,7 +299,7 @@ cupsJSONExportString(cups_json_t *json)	// I - JSON root node
     }
     else if (current->sibling)
     {
-      // Visit silbling
+      // Visit sibling
       current = current->sibling;
     }
     else
@@ -444,7 +444,7 @@ cupsJSONExportString(cups_json_t *json)	// I - JSON root node
 
       if (current->sibling)
       {
-	// Visit silbling
+	// Visit sibling
 	current = current->sibling;
       }
       else if ((current = current->parent) != NULL)
