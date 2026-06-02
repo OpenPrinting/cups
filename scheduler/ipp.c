08f531e75a8e55e252a600807601e240a3c3c2fc
abubakarsabir924-cell <abubakarsabir924@gmail.com>
2026-06-02 01:57:04 -0400

    Fix segfault: add send_ipp_status before goto add_printer_attributes

--- scheduler/ipp.c
+++ scheduler/ipp.c
@@ -5745,6 +5745,7 @@ create_local_printer(
           cupsdLogMessage(CUPSD_LOG_DEBUG, "create_local_printer: FOUND MATCHING PERMANENT QUEUE: %s", perm->name);
           printer = perm;
                     printer->state_time = time(NULL);
+                    send_ipp_status(con, IPP_STATUS_OK, _("Printer \"%s\" already exists."), printer->name);
                     goto add_printer_attributes;
         }
       }
