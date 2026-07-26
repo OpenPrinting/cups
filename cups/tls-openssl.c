75b820172a7080f47847680885958c7e5af3b765
Michael R Sweet <msweet@msweet.org>
2026-07-26 18:10:47 -0400

    Fix code spacing.

--- cups/tls-openssl.c
+++ cups/tls-openssl.c
@@ -100,6 +100,7 @@ cupsAreCredentialsValidForName(
 
     if (X509_NAME_get_text_by_NID(X509_get_subject_name(cert), NID_commonName, subjectName, sizeof(subjectName)) < 0)
       cupsCopyString(subjectName, "unknown", sizeof(subjectName));
+
     DEBUG_printf("1cupsAreCredentialsValidForName: subjectName=\"%s\"", subjectName);
 
     if (!_cups_strcasecmp(common_name, subjectName))
@@ -110,8 +111,10 @@ cupsAreCredentialsValidForName(
 
 #ifdef DEBUG
     char issuerName[256];
+
     if (X509_NAME_get_text_by_NID(X509_get_issuer_name(cert), NID_commonName, issuerName, sizeof(issuerName)) < 0)
       cupsCopyString(issuerName, "unknown", sizeof(issuerName));
+
     DEBUG_printf("1cupsAreCredentialsValidForName: issuerName=\"%s\"", issuerName);
 #endif // DEBUG
 
@@ -803,8 +806,10 @@ cupsGetCredentialsInfo(
 
     if (X509_NAME_get_text_by_NID(X509_get_subject_name(cert), NID_commonName, name, sizeof(name)) < 0)
       cupsCopyString(name, "unknown", sizeof(name));
+
     if (X509_NAME_get_text_by_NID(X509_get_issuer_name(cert), NID_commonName, issuer, sizeof(issuer)) < 0)
       cupsCopyString(issuer, "unknown", sizeof(issuer));
+
     expiration = openssl_get_date(cert, 1);
 
     switch (X509_get_signature_nid(cert))
@@ -1510,10 +1515,13 @@ httpCopyPeerCredentials(http_t *http)	// I - Connection to server
 
 #ifdef DEBUG
 	char subjectName[256], issuerName[256];
+
 	if (X509_NAME_get_text_by_NID(X509_get_subject_name(cert), NID_commonName, subjectName, sizeof(subjectName)) < 0)
 	  cupsCopyString(subjectName, "unknown", sizeof(subjectName));
+
 	if (X509_NAME_get_text_by_NID(X509_get_issuer_name(cert), NID_commonName, issuerName, sizeof(issuerName)) < 0)
 	  cupsCopyString(issuerName, "unknown", sizeof(issuerName));
+
 	DEBUG_printf("1httpCopyPeerCredentials: subjectName=\"%s\", issuerName=\"%s\"", subjectName, issuerName);
 
 	STACK_OF(GENERAL_NAME) *names;	// subjectAltName values
