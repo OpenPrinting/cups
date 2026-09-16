65b44e423665e73614297c5305cfa32d7de50df6
Michael R Sweet <msweet@msweet.org>
2026-09-16 17:07:47 -0400

    Fix device authorization grant code - only set retry time for authorization_pending error.

--- cups/oauth.c
+++ cups/oauth.c
@@ -1334,7 +1334,7 @@ cupsOAuthGetTokens(
 
   if (error)
   {
-    if (access_expires)
+    if (!strcmp(error, "authorization_pending") && access_expires)
     {
       // Handle "soft" device access token errors by setting access_expires to
       // the next call time...
@@ -1837,6 +1837,8 @@ oauth_copy_response(http_t *http)	// I - HTTP connection
   if (httpGetState(http) == initial_state)
     httpFlush(http);
 
+  DEBUG_printf("3oauth_copy_response: Returning \"%s\".", body);
+
   return (body);
 }
 
@@ -1936,7 +1938,10 @@ oauth_do_post(const char *ep,		// I - Endpoint URI
 
   // Connect to the endpoint...
   if ((http = httpConnectURI(ep, host, sizeof(host), &port, resource, sizeof(resource), /*blocking*/true, /*msec*/30000, /*cancel*/NULL, /*require_ca*/true)) == NULL)
+  {
+    DEBUG_puts("4oauth_do_post: Unable to connect to endpoint.");
     return (NULL);
+  }
 
   // Send a POST request with the request data...
   req_length = strlen(request);
@@ -1948,18 +1953,32 @@ oauth_do_post(const char *ep,		// I - Endpoint URI
 
   if (!httpWriteRequest(http, "POST", resource))
   {
+    DEBUG_puts("4oauth_do_post: POST failed.");
+
     if (!httpConnectAgain(http, 30000, NULL))
+    {
+      DEBUG_puts("4oauth_do_post: Reconnect failed.");
       goto done;
+    }
 
     if (!httpWriteRequest(http, "POST", resource))
+    {
+      DEBUG_puts("4oauth_do_post: Second POST failed.");
       goto done;
+    }
   }
 
   if (httpWrite(http, request, req_length) < (ssize_t)req_length)
+  {
+    DEBUG_puts("4oauth_do_post: Write of request body failed.");
     goto done;
+  }
 
   // Get the response...
-  while ((status = httpUpdate(http)) == HTTP_STATUS_CONTINUE);
+  while ((status = httpUpdate(http)) == HTTP_STATUS_CONTINUE)
+    ;
+
+  DEBUG_printf("4oauth_do_post: Got status %d.", status);
 
   response  = oauth_copy_response(http);
   resp_json = cupsJSONImportString(response);
@@ -1984,6 +2003,8 @@ oauth_do_post(const char *ep,		// I - Endpoint URI
   done:
 
   httpClose(http);
+
+  DEBUG_printf("4oauth_do_post: Returning %p.", (void *)resp_json);
 
   return (resp_json);
 }
@@ -2372,6 +2393,8 @@ oauth_set_error(cups_json_t   *json,	// I - JSON response
     error      = cupsGetOption("error", num_form, form);
     error_desc = cupsGetOption("error_description", num_form, form);
   }
+
+  DEBUG_printf("3oauth_set_error: error=\"%s\", error_description=\"%s\"", error, error_desc);
 
   if (error && strcmp(error, "authorization_pending") && strcmp(error, "slow_down"))
   {
